dbacc83448c56755b93089a83fc31279966bca32
Rujal Tamrakar <38482136+rujal404@users.noreply.github.com>
2026-03-24 15:42:41 +0545

    links modified

---
+++ assets/js/inspectelement.js
@@ -0,0 +1,28 @@
+// Inspect disable
+
+    document.addEventListener("contextmenu", function(event){
+      // alert("Inspect Elements Not Allowed");
+      event.preventDefault();
+    })
+    document.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+  }
+  );
+  document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+Us
